8b48afe88d34faa1bc08fb56c08cd73c32c176de
Fabian <fabian@prueba.com>
2023-10-16 19:43:48 +0200

    Created controller -- IT DOESN'T WORK

---
+++ controller/pedidoController.php
@@ -0,0 +1,15 @@
+<?php
+// Creating controller for comands
+
+class pedidoController
+{
+    public function index()
+    {
+        echo 'Pagina principal pedidos';
+    }
+
+    public function compra()
+    {
+        echo 'Pagina de compra';
+    }
+}
--- index.php
+++ index.php
@@ -1,5 +1,40 @@
 <?php
+include_once 'controller/pedidoController.php';
+include_once 'config/parameters.php';
 
-echo 'esto es tu indice';
+if(!isset($_GET['controller']))
+{
+    // si no se pasa nada se mostrará pagina principal de pedidos.
+    header("Location: " . URL . "?controller=pedido");
+}
+else
+{
+    $nombre_controller = $_GET['controller'] . 'Controller';
+
+    if (class_exists($nombre_controller))
+    {
+        // miramos si nos pasa una accion
+        // en caso contrario mostramos accion por defecto
+
+        $controller = new $nombre_controller;
+
+        if (isset($_GET['action']))
+        {
+            $action = $_GET['action'];
+        }
+        else
+        {
+            $action = 'index';
+        }
+
+        $controller->$action();
+    }
+    else
+    {
+        header("Location:".URL."?controller=pedido");
+        echo $nombre_controller . ' no existe';
+    }
+}
+
 
 ?>
