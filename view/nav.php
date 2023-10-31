53b2f1b1785d19baa9d1ddcafc541ad1d2769626
Fabian <fabian@prueba.com>
2023-10-31 17:36:39 +0100

    IRN=Set enviroment for bootstrap

--- view/nav.php
+++ view/nav.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/nav-style.css">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Tienda online | Restaurante Conforama</title>
+  
+    <link href="../assets/style/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/style/full_estil.css" rel="stylesheet" type="text/css" media="screen">
 
 </head>
 <body>
@@ -33,4 +37,7 @@
         </nav>
     </div>
 </body>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
 </html>
