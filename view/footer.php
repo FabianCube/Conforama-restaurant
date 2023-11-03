174734e07547ab4b08eaead8746ae1f1a8f1772b
Fabian <fabian@prueba.com>
2023-11-03 17:52:21 +0100

    IRN=New footer file

---
+++ view/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+    <footer class="container-fluid">
+        <section class="container bg-primary" style="width: 100%;">
+            hola
+        </section>
+    </footer>
+</body>
+</html>
