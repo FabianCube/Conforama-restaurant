10514974e6a145b62a43624f9cbec400428d9838
Fabian <fabian@prueba.com>
2023-11-07 17:30:17 +0100

    Added auto slider carrusel in home page

--- view/header.php
+++ view/header.php
@@ -5,12 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="assets/style/header.css">
 </head>
 
 <body>
-    <header class="" style="margin-top: 55px;">
-        <div class="d-flex justify-content-center" style="background-color: rgba(0, 0, 0, 0.6);">
-            <img class="img-fluid" src="assets/images/header.png" alt="" style="width: 100%; height: auto;">
+    <header style="margin-top: 55px;">
+        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="custom-background-1"></div>
+                </div>
+                <div class="carousel-item">
+                    <div class="custom-background-2"></div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </header>
 </body>
