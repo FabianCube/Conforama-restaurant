d9fdd4509ae88e0852da94c3fdfa42f70dd91ff9
Fabian <fabian@prueba.com>
2023-11-13 20:09:57 +0100

    styled footer

--- view/footer.php
+++ view/footer.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
+
+    <link rel="stylesheet" href="assets/style/footer.css">
 </head>
+
 <body>
     <footer class="mt-5">
-        <section class="bg-secondary" style="width: 100%; height: 100px;">
-            <p>FOOTER</p>
+        <section class="custom-content-parent">
+            <div class="container d-flex justify-content-center align-items-center flex-column" style="height: 100%;">
+                <div class="row mb-4" style="text-align: center;">
+                    <div class="col"><img class="custom-image-footer" src="<?= image_url ?>twitter.png" alt="Icono Twitter"></div>
+                    <div class="col"><img class="custom-image-footer" src="<?= image_url ?>facebook.png" alt="Icono Facebook"></div>
+                    <div class="col"><img class="custom-image-footer" src="<?= image_url ?>instagram.png" alt="Icono Instagram"></div>
+                    <div class="col"><img class="custom-image-footer" src="<?= image_url ?>youtube.png" alt="Icono Youtube"></div>
+                    <div class="col"><img class="custom-image-footer" src="<?= image_url ?>pinterest.png" alt="Icono Pinterest"></div>
+                </div>
+                <div class="row" style="text-align: center;">
+                    <p class="text-danger">Copyright 2023 | Fabian Doizi Bonilla</p>
+                </div>
+            </div>
         </section>
     </footer>
 </body>
+
 </html>
