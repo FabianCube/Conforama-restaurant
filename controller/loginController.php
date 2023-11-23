4656db16e8f4f90ae1f4ba424c1dcb47aa9f1d09
Fabian <fabian@prueba.com>
2023-11-23 17:57:56 +0100

    implementing login

--- controller/loginController.php
+++ controller/loginController.php
@@ -1,11 +1,23 @@
 <?php
 
+include_once 'model/UsuariosDAO.php';
+
 class loginController
 {
     public function index()
     {
+        session_start();
         include_once 'view/nav.php';
         include_once 'view/login.php';
         include_once 'view/footer.php';
+    }
+
+    public static function login()
+    {
+        // get the email and password
+
+        // check if the data exist in the db
+
+        // redirect to the account
     }
 }
