b7edffb6c4abc6da7ecad5a91c2bd6ca63ef20a7
Fabian <fabian@prueba.com>
2023-11-23 16:40:19 +0100

    addign login

---
+++ controller/loginController.php
@@ -0,0 +1,11 @@
+<?php
+
+class loginController
+{
+    public function index()
+    {
+        include_once 'view/nav.php';
+        include_once 'view/login.php';
+        include_once 'view/footer.php';
+    }
+}
---
+++ view/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login / Register</title>
+</head>
+<body>
+    <section class="container" style="max-width: 1170px;">
+        <div class="row bg-primary">
+            <p>hola</p>
+        </div>
+        <div class="row bg-primary">
+            <p>hola</p>
+        </div>
+        <div class="row bg-primary">
+            <p>hola</p>
+        </div>
+    </section>
+</body>
+</html>
