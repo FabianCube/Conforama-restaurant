2f060c7237f2d6efc54efb31101758c854a48ec5
Fabian D <fabiandoizifp@ibf.cat>
2023-12-18 16:43:43 +0100

    herencia de caca

---
+++ model/Cliente.php
@@ -0,0 +1,29 @@
+<?php
+
+class Cliente extends Usuarios
+{
+    private $nivel;
+
+    public function __construct()
+    { }
+
+    /**
+     * Get the value of nivel
+     */ 
+    public function getNivel()
+    {
+        return $this->nivel;
+    }
+
+    /**
+     * Set the value of nivel
+     *
+     * @return  self
+     */ 
+    public function setNivel($nivel)
+    {
+        $this->nivel = $nivel;
+
+        return $this;
+    }
+}
