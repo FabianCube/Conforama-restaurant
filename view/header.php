f990dff5639dee7c630422f5b7fb5b927d8d1787
Fabian D <fabiandoizifp@ibf.cat>
2024-01-07 16:19:39 +0100

    responsive header

--- view/header.php
+++ view/header.php
@@ -12,14 +12,19 @@
 <body>
     <header style="margin-top: 55px;">
         <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+
             <div class="carousel-inner">
+                
                 <div class="carousel-item active">
-                    <div class="custom-background-1"></div>
+                    <img src="<?= image_url . "header.png" ?>" class="d-block w-100" alt="Image header conforama">
                 </div>
+
                 <div class="carousel-item">
-                    <div class="custom-background-2"></div>
+                    <img src="<?= image_url . "header-2.png" ?>" class="d-block w-100" alt="Image header conforama">
                 </div>
+
             </div>
+
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
